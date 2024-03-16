36cc5782e8e20107260d14f7c1abdab27d892f18
davidrockkj <davidrockkj@gmail.com>
2024-03-16 19:32:10 -0300

    Implementando 'Button' em 'Home'

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom'
 
+import { Button } from '../../components/Button'
+
 const Home = () => {
   return (
     <>
       <h1>Home</h1>
+      <Button title='Teste 1' />
+      <Button title='Teste 2' variant='secondary' />
       <Link to='/login'>Fazer login</Link>
     </>
   )
