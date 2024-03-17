291d78fcc81cfe95e306c04e54ee9d04bd8a9ae5
David Rocha <davidrockkj@gmail.com>
2024-03-17 16:49:26 -0300

    Começando a estruturar 'Header' e adicionando 'react-scripts' ao projeto (só tinha ele de maneira global)

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import {
+  HeaderContainer,
+  Row,
+  Column,
+  Wrapper,
+  BuscarInputContainer,
+  Menu,
+  MenuRight,
+  UserPicture,
+  Input
+} from "./styles"
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <HeaderContainer>
+        <Row>
+          
+        </Row>
+      </HeaderContainer>
+    </Wrapper>
+  )
+}
+
+export { Header }
