eefc240a82be1e2e6684f69dc77f44eb2dd825bb
David Rocha <davidrockkj@gmail.com>
2024-03-17 19:16:40 -0300

    Login: implementando componentes

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,15 +1,19 @@
 // import { Link } from 'react-router-dom'
 
-import banner from '../../assets/banner.png'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 
 import {
+  Column,
   Container,
+  CriarText,
+  EsqueciText,
+  Row,
+  SubLogin,
   Title,
-  TitleHighlight,
-  TextContent
+  TitleLogin,
+  Wrapper
 } from './styles'
 
 const Login = () => {
@@ -17,26 +21,26 @@ const Login = () => {
     <>
       <Header />
       <Container>
-        <div>
+        <Column>
           <Title>
-            <TitleHighlight>
-              Implemente
-              <br/>
-            </TitleHighlight>
-            o seu futuro global agora!  
+            A plataforma para você aprender com experts, dominar as principais tecnologias e entrar mais rápido nas empresas mais desejadas.
           </Title>
-          <TextContent>
-            Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare seu novo desafio profissional, evoluindo em comunidade com os melhores experts.
-          </TextContent>
-          <Button
-            title='Começar agora'
-            variant='secundary'
-            onClick={ () => null }
-          />
-        </div>
-        <div>
-          <Input placeholder='email' />
-        </div>
+        </Column>
+        <Column>
+          <Wrapper>
+            <TitleLogin>Faça seu cadastro</TitleLogin>
+            <TitleLogin>Faça seu login e make the change._</TitleLogin>
+            <form>
+              <Input placeholder='Email' />
+              <Input placeholder='Senha' type='password' />
+              <Button title='Entrar' variant='secundary' />
+            </form>
+            <Row>
+              <EsqueciText>Esqueci minha senha</EsqueciText>
+              <CriarText>Criar conta</CriarText>
+            </Row>
+          </Wrapper>
+        </Column>
       </Container>
     </>
   )
