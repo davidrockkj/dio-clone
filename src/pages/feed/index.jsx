9c75b40343314102ae657ea9066171a62ee5ce76
davidrockkj <davidrockkj@gmail.com>
2024-03-18 12:07:43 -0300

    feed: implementando 'UserInfo'

--- src/pages/feed/index.jsx
+++ src/pages/feed/index.jsx
@@ -1,6 +1,7 @@
 // import { Link } from 'react-router-dom'
 
 import { Card } from '../../components/Card'
+import { UserInfo } from '../../components/UserInfo'
 import { Header } from '../../components/Header'
 
 import {
@@ -13,6 +14,7 @@ const Feed = () => {
       <Header />
       <Container>
         <Card />
+        <UserInfo percentual={55} nome='David Rocha' image={'https://avatars.githubusercontent.com/u/62350916?v=4'} />
       </Container>
     </>
   )
