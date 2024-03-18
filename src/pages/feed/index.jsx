a6a34c57c385e5957631d6561a4f472135d49902
davidrockkj <davidrockkj@gmail.com>
2024-03-18 12:15:23 -0300

    feed: estruturando

--- src/pages/feed/index.jsx
+++ src/pages/feed/index.jsx
@@ -6,6 +6,9 @@ import { Header } from '../../components/Header'
 
 import {
   Container,
+  Column,
+  Title,
+  TitleHighlight
 } from './styles'
 
 const Feed = () => {
@@ -13,8 +16,23 @@ const Feed = () => {
     <>
       <Header />
       <Container>
-        <Card />
-        <UserInfo percentual={55} nome='David Rocha' image={'https://avatars.githubusercontent.com/u/62350916?v=4'} />
+        <Column flex={3}>
+          <Title>Feed</Title>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </Column>
+        <Column flex={1}>
+          <TitleHighlight># RANKING 5 TOP DA SEMANA</TitleHighlight>
+          <UserInfo percentual={80} nome="David Rocha" image={'https://avatars.githubusercontent.com/u/62350916?v=4'} />
+          <UserInfo percentual={27} nome="David Rocha" image={'https://avatars.githubusercontent.com/u/62350916?v=4'} />
+          <UserInfo percentual={94} nome="David Rocha" image={'https://avatars.githubusercontent.com/u/62350916?v=4'} />
+          <UserInfo percentual={15} nome="David Rocha" image={'https://avatars.githubusercontent.com/u/62350916?v=4'} />
+          <UserInfo percentual={25} nome="David Rocha" image={'https://avatars.githubusercontent.com/u/62350916?v=4'} />
+        </Column>
       </Container>
     </>
   )
