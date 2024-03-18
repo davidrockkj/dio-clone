5b687f7d6b395ac6c700502afe0725068549f44f
davidrockkj <davidrockkj@gmail.com>
2024-03-18 12:19:22 -0300

    Header: verificando autenticação para definir o que será mostrado

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,28 +5,41 @@ import {
   BuscarInputContainer,
   Menu,
   MenuRight,
-  Input
+  Input,
+  UserPicture
 } from "./styles"
 import { Button } from "../Button"
 
 import logo from '../../assets/logo-dio.png';
 
-const Header = () => {
+const Header = ({ autenticado }) => {
   return (
     <Wrapper>
       <HeaderContainer>
         <Row>
           <img src={logo} alt="Logo da dio" />
-          <BuscarInputContainer>
-            <Input placeholder="Buscar..." />
-          </BuscarInputContainer>
-          <Menu>Live Code</Menu>
-          <Menu>Global</Menu>
+          {autenticado ? (
+            <>
+              <BuscarInputContainer>
+                <Input placeholder="Buscar..." />
+              </BuscarInputContainer>
+              <Menu>Live Code</Menu>
+              <Menu>Global</Menu>
+            </>
+          ) : null}
         </Row>
         <Row>
-          <MenuRight href='#'>Home</MenuRight>
-          <Button title='Entrar' />
-          <Button title='Cadastrar' />
+          {autenticado ? (
+            <>
+              <UserPicture src="https://avatars.githubusercontent.com/u/62350916?v=4" />
+            </>
+          ) : (
+            <>
+              <MenuRight href='#'>Home</MenuRight>
+              <Button title='Entrar' />
+              <Button title='Cadastrar' />
+            </>
+          )}
         </Row>
       </HeaderContainer>
     </Wrapper>
